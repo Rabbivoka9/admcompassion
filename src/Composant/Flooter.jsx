4a9a8d5a5762bdb8a93e63ac1b6bc663ad1aa568
Rabbivoka9 <rabbivoka9@gmail.com>
2023-07-04 16:27:11 +0200

    ajour

--- src/Composant/Flooter.jsx
+++ src/Composant/Flooter.jsx
@@ -5,12 +5,12 @@ function Pied (){
     return(
         <div>
             <Footer className="footer">
-            <p className="foot"> Administaration-Crefm-2023</p>
+            {/* <p className="foot"> Administaration-Crefm-2023</p>
             <div className="cont-icon">
                 <img className="icon" src="twitter(3).png" alt="" />
                 <img className="icon" src="instagram(6).png" alt="" />
                 <img className="icon" src="facebook(6).png" alt="" />
-            </div>
+            </div> */}
         </Footer>
         </div>
         
